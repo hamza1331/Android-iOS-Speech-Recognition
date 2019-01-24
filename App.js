8f281d82f686f73a80982fe67944dcffebd23261
Hamza Ali <hamxa1331@gmail.com>
2019-01-25 04:58:48 +0500

    Recognizing basic command
    Working on recognizing basic command after speech synthesis.

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import {
   Text,
   View,
   TouchableOpacity,
+  Alert
 } from 'react-native';
+import { dates,events,weekdays,keywords, months,dayNames } from "./Data";
 import Voice from 'react-native-voice';
 export default class App extends React.Component {
   constructor(props) {
@@ -13,6 +15,7 @@ export default class App extends React.Component {
       recognized: '',
       started: '',
       results: [],
+      fragments:''
     };
 Voice.onSpeechStart = this.onSpeechStart.bind(this);
     Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
@@ -35,6 +38,53 @@ onSpeechResults(e) {
     this.setState({
       results: e.value,
     });
+    var fragments = e.value.join()
+    this.setState({
+      fragments
+    })
+    let frag = fragments.trim().split(/\s+/)
+    let valid = keywords.find((word)=>{
+      return frag.includes(word)?true:false
+    })
+    console.log(valid)
+    if(valid){
+      let cmd = {}
+      frag.forEach(word=>{
+
+        let weekday = weekdays.find((week)=>word.toLowerCase()===week)
+        if(weekday){
+          cmd.weekday=weekday
+        }
+        let date = dates.find(date=>date==word)
+        if(date){
+          cmd.date = date
+        }
+        let month = months.find(m=>m===word.toLowerCase())
+        if(month){
+          cmd.month = month
+        }
+        let indexOfFor = word.indexOf("for")
+        let persons = word[+indexOfFor]
+        if(persons){
+          cmd.persons = persons
+        }
+        let event = events.find(e=>e===word.toLowerCase())
+        if(event){
+          cmd.event = event
+        }
+        else{
+          cmd.event = "general"
+        }
+        let dayName = dayNames.find(day=>day===word.toLowerCase())
+        if(dayName){
+          cmd.dayName = dayName
+        }
+      })
+      console.log(cmd)
+      }
+      else{
+        Alert.alert('Failed',"Couldn't Recognize Commmand")
+      }
   }
 async _startRecognition(e) {
     this.setState({
@@ -55,8 +105,9 @@ render () {
         <Text style={styles.transcript}>
             Transcripts
         </Text>
-        {this.state.results.map((result, index) => <Text key={index} style={styles.transcript}> {result}</Text>
-        )}
+        {/* {this.state.results.map((result, index) => <Text key={index} style={styles.transcript}> {result}</Text>
+        )} */}
+        {this.state.fragments.length>0&&<Text style={styles.transcript}>{this.state.fragments}</Text>}
         <TouchableOpacity
        style={{
          borderWidth:1,
---
+++ Data.js
@@ -0,0 +1,14 @@
+const weekdays=["monday","tuesday","wednesay","thursday","friday","saturday","sunday"]
+const events = ["date","birthday","party","meeting","lunch","brunch","breakfast","dinner","tea"]
+const months=["january","february","march","april","may","june","july","august",'september',"october","november","december"]
+const dates = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+const keywords=["Reserve","Book","Retain","Arrange","arrange","retain",'book']
+const dayNames = ["tomorrow","weekend"]
+export{
+    weekdays,
+    events,
+    months,
+    dates,
+    keywords,
+    dayNames
+}
